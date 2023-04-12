3762916423d720de21098c55e25cd500b1155df5
Butcherok <butcherok231@gmail.com>
2023-04-12 22:44:26 +0300

    add base Routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import MoviePage from './Pages/MoviePage';
+import Layout from './Layout/Layout';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="movies" element={<MoviePage />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
